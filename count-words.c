9205206dd9c0e7446d8bdaf24a40f1756d0ffd85
tamaricoh <tamarcmail@gmail.com>
2024-05-15 12:55:53 +0300

    Finish part 0

--- count-words.c
+++ count-words.c
@@ -7,29 +7,32 @@
 /* return string "word" if the count is 1 or "words" otherwise */
 char *words(int count)
 {
-  char *words = "words";
-  if (count == 1)
-    	words[strlen(words)-1] = '\0';
-  
-  return words;
+    // Fix the problem - from "words" to strdup("words")
+    // Problem - we attempted to modify the contents of a string we are not allowed to change
+    // Solution - duplicate
+    char *words = strdup("words");
+    if (count == 1){
+        words[strlen(words)-1] = '\0';
+    }
+    return words;
 }
 
 /* print a message reportint the number of words */
 int print_word_count(char **argv)
 {
-  int count = 0;
-  char **a = argv;
-  while (*(a++))
-    ++count;
-  char *wordss = words(count);
-  printf("The sentence contains %d %s.\n", count, wordss);
-  
-  return count;
+    int count = 0;
+    char **a = argv;
+    while (*(a++))
+        ++count;
+    char *wordss = words(count);
+    printf("The sentence contains %d %s.\n", count, wordss);
+
+    return count;
 }
 
 /* print the number of words in the command line and return the number as the exit code */
 int main(int argc, char **argv)
 {
-  print_word_count(argv + 1);
-  return 0;
+    print_word_count(argv + 1);
+    return 0;
 }
